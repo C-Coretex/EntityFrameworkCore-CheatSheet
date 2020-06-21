c3dd09bd8431707d4d031e1e7008ab05cb660a83
C-Coretex <valerash2@gmail.com>
2020-06-21 15:39:42 +0300

    Added orderby example

--- Program.cs
+++ Program.cs
@@ -18,53 +18,66 @@ namespace EFTest
             
             using(DataEntity context = new DataEntity())
             {
-            //Add object to the regular table
-                //context.Table.Add(new Table { Name = "Test1"});
+				//Add object to the regular table
+				//context.Table.Add(new Table { Name = "Test1"});
 
 
-            //Delete object
-                //Table table = context.Table.FirstOrDefault(r => r.Name == "SecondTest");
-                //context.Table.Remove(table);
+				//Delete object
+				//Table table = context.Table.FirstOrDefault(r => r.Name == "SecondTest");
+				//context.Table.Remove(table);
 
 
-            //Add object to the table with the foreign key
-                //Category category = context.Category.FirstOrDefault(c => c.Name == "Breakfast");
-                //category.Table.Add(new Table{ Name="Tetst2" });
+				//Add object to the table with the foreign key
+				//Category category = context.Category.FirstOrDefault(c => c.Name == "Breakfast");
+				//category.Table.Add(new Table{ Name="Tetst2" });
 
 
-            //Select objects that have "Breakfast" category + LINQ Method Syntax
-                //Category category = context.Category.Include(x=>x.Table).FirstOrDefault(c => c.Name == "Breakfast");
-                //List<Table> tables = category.Table.ToList();
-            //or like this    
-                //var tables = context.Table.Where(t => t.Category.Name == "Breakfast").ToList();
+				//Select objects that have "Breakfast" category + LINQ Method Syntax
+				//Category category = context.Category.Include(x=>x.Table).FirstOrDefault(c => c.Name == "Breakfast");
+				//List<Table> tables = category.Table.ToList();
+				//or like this    
+				//var tables = context.Table.Where(t => t.Category.Name == "Breakfast").ToList();
+
+				//tables.ForEach(r => Console.WriteLine(r.Name));
+
+
+				//Select objects that have "Breakfast" category + LINQ Query Syntax
+				//var tablesQuery = (from t in context.Table
+				//                   where t.Category.Name == "Breakfast"
+				//                   select t).ToList();
+
+				//tablesQuery.ForEach(t  => Console.WriteLine((t as Table).Name));
+
+
+				//Write category of the object
+				//Table tableT = context.Table.FirstOrDefault(r => r.Name == "Tetst3");
+				//Console.WriteLine(tableT.Category.Name);
+
+
+				//Proper way to get an object from database(as I think)
+				//Table table = context.Table.FirstOrDefault(r => r.Name == "SuperFirst");
+				//if(table != null)
+				//Console.WriteLine($"ID {table.Id} - CATEGORY {table.Category.Name} - old NAME {table.Name.Trim()} - new NAME {table.Name = "FirstTest"}");
+				//else
+				//Console.WriteLine("table is null");
+
                 
-                //tables.ForEach(r => Console.WriteLine(r.Name)); //Returns nothing (tables.Count == 0)
+                //Proper way to sort objects if are possible diferent sorting methods
+                //bool isAscending = false;
 
-
-            //Select objects that have "Breakfast" category + LINQ Query Syntax
-                //var tablesQuery = (from t in context.Table
-                //                   where t.Category.Name == "Breakfast"
-                //                   select t).ToList();
-                
-                //tablesQuery.ForEach(t  => Console.WriteLine((t as Table).Name)); //Returns proper data (tables.Count == 2)
-
-
-            //Write category of the object
-                //Table tableT = context.Table.FirstOrDefault(r => r.Name == "Tetst3");
-                //Console.WriteLine(tableT.Category.Name);
-
-
-            //Proper way to get an object from database(as I think)
-                //Table table = context.Table.FirstOrDefault(r => r.Name == "SuperFirst");
-                //if(table != null)
-                //Console.WriteLine($"ID {table.Id} - CATEGORY {table.Category.Name} - old NAME {table.Name.Trim()} - new NAME {table.Name = "FirstTest"}");
+				//IQueryable<Table> queryTables = (from t in context.Table
+				//					        select t);
+                //List<Table> tables;
+				//if(isAscending)
+				//    tables = queryTables.OrderBy(t => t.Name).ToList();
                 //else
-                //Console.WriteLine("table is null");
-
-
-            //Save Changes at the end
-                //context.SaveChanges();
-            }
+                //    tables = queryTables.OrderByDescending(t => t.Name).ToList();
+				
+               //tables.ForEach(r => Console.WriteLine(r.Name));
+                
+				//Save Changes at the end
+				//context.SaveChanges();
+			}
 
             
             IList<string> stringList = new List<string>() 
